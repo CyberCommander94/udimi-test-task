b5c6337b1c63ffff8e84903b23052ef5e570bffe
Oleh Karpovych <oleg.karpovich.94@gmail.com>
2023-04-27 00:31:33 +0300

    51448e9

---
+++ _nuxt/static/1682544641/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"udimi-test-task\u002F_nuxt\u002Fstatic\u002F1682544641",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"udimi-test-task\u002F",assetsPath:"udimi-test-task\u002F_nuxt\u002F",cdnURL:a}}}}(null));
